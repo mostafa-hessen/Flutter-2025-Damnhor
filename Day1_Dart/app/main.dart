7507b88fab63237206cc01846480492db7bce631
mostafa-hessen <mb4952144@gmail.com>
2025-05-13 09:33:15 +0300

    flutter day2

---
+++ Day1_Dart/app/main.dart
@@ -0,0 +1,70 @@
+void main() {
+ 
+ String usrName= "Mostafa";
+
+ String inputUser= "Mostafa";
+
+print(usrName.codeUnits);
+print(inputUser.codeUnits);
+  print(usrName.compareTo(inputUser)); // 1 - -1  - 0
+
+}
+
+
+/*
+ **operators**
+
+ 1- Arithmatic => + - / * %
+ 2- logical operatore => || && == 
+ 3- increment and decremetn  => ++x , x++ , --X , x--
+ 4- assignment  => += -= *= ==> x+=1 => x= x+1
+
+
+
+*/ 
+/*
+
+data types
+
+[int ,double ==> num]
+
+String 
+
+bool => true - flase
+
+
+  // int number1= 1224566;
+
+  // double salary=10.5;
+
+  // num id =1234;
+
+  // String name="mostafa";
+
+  // bool isStrudent= false;
+
+
+  // List<int> arr=[1,2,3,4,5];
+
+  // print("hello my name is $name my id is $id  ${number1+salary}");
+*/ 
+
+    
+// /*
+// //  increment ++  - decrement --
+//  post => car++  - pre => ++car 
+// */ 
+//  int car =10 ;
+//  print('in 9 ==>${++car}');
+//  print('after  9 ==> $car');
+
+
+
+
+
+
+
+
+
+
+
