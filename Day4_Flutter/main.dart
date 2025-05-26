7031a5459100c22d09be1f498bdca33f895e6b07
mostafa-hessen <mb4952144@gmail.com>
2025-05-27 02:12:01 +0300

    day5

---
+++ Day4_Flutter/main.dart
@@ -0,0 +1,96 @@
+void main() {
+
+
+ 
+}
+
+
+
+// abstract class Charger {
+//   charger();
+// }
+
+// // موبايل - ايفون - ساعه - لاب توب
+
+// class Mobil implements Charger  {
+  
+
+// }
+
+// class Watch  implements Charger  {
+
+// }
+
+// class Laptop  implements Charger  {}
+
+// St
+// printDayType("sunday");
+// }
+
+// // ❌ الطريقة التقليدية (بدون enum)
+// void printDayType(String day) {
+//   if (day == "Saturday" || day == "Sunday") {
+//     print("يوم عطلة");
+//   } else {
+//     print("يوم عمل");
+//   }
+// }
+
+//  abstract class Shap {
+//   color(){
+//     print("color Black");
+//   }
+
+//   area();
+// }
+
+// class Circle extends Shap {
+//   @override
+//   area() {
+
+//   }
+
+// }
+
+// class Rectangle extends Shap {
+//   @override
+//   area() {
+//     // TODO: implement area
+
+//   }
+
+// }
+
+// class Animal {
+//   String? name;
+//   String? kingdom;
+//   eat(){
+//     print("eating");
+//   }
+// }
+
+// mixin Fly{
+//   fly(){}
+
+// }
+
+// mixin Swimming{
+//   swim(){}
+// }
+
+// mixin Walk{
+
+//   walk(){}
+// }
+
+// class Dog  extends Animal with Swimming , Walk { // swimm -walk
+
+// }
+
+// class Dolphin extends Animal with  Swimming{  // swim
+
+// }
+
+// class Duck  extends Animal with  Walk,Swimming , Fly{ // swimm - fly - walk
+
+// }
---
+++ day1_flutter/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("flutter damnhore"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+            // justifyContent
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          children: [
+           
+          
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/*
+
+
+
+*/ 
+
+
+
+
